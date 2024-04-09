04958ddbb4c771ba3ebe64b02149605e1d7a90f9
Karhopsis <yaryk23041997@gmail.com>
2024-04-09 16:05:14 +0300

    day 6 - 2

---
+++ service/service/tasks.py
@@ -0,0 +1,13 @@
+from celery import shared_task
+from django.db.models import Prefetch, F
+
+@shared_task
+def set_price(subscription_id):
+    from services.models import Subscription
+    subscription = Subscription.objects.filter(id=subscription_id).annotate(price=F('service__full_price') -
+                       F('service__full_price')*F('plan__discount_percent')/100.00).first()
+
+
+    new_price = (subscription.service.full_price)-(subscription.service.full_price*subscription.plan.discount_percent/100)
+    subscription.price= new_price
+    subscription.save()
